b176181696b560e905cdb0b4095c61f6eaff5074
chick_0 <chick_0@kakao.com>
2020-09-19 01:35:29 +0900

    1.1 - 블루프린트 등록 방식 변경

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
+import importlib
+
 from flask import Flask
 
 
@@ -7,8 +10,14 @@ def create_app():
     app = Flask(__name__)
 
     # API
-    from .views import epost
+    for m in os.listdir(os.path.join("app", "views")):
+        if not m.startswith("__"):
+            module = importlib.import_module(
+                name=f"app.views.{m.split('.py')[0]}"
+            )
 
-    app.register_blueprint(epost.bp)
+            app.register_blueprint(
+                blueprint=module.__getattribute__("bp")
+            )
 
     return app
